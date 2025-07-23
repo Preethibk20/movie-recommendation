d84559aabf5cf122a1dfd07839694e6e830c7402
Preethibk20 <138660369+Preethibk20@users.noreply.github.com>
2025-07-23 22:41:51 +0530

    Update script.js

--- client/script.js
+++ client/script.js
@@ -1,5 +1,5 @@
 // Replace with your actual Render backend URL!
-const API_BASE = "https://movie-recommendation-olxaonrender_com/api/movies";
+const API_BASE = "https://movie-recommendation-olxaonrender.com/api/movies";
 
 function searchMovies() {
   const title = document.getElementById("searchInput").value;
