994346f55ea6ab9d72f0db9c0a6e4445a0567043
Preethibk20 <138660369+Preethibk20@users.noreply.github.com>
2025-07-23 21:52:35 +0530

    Update movieRoutes.js

--- server/routes/movieRoutes.js
+++ server/routes/movieRoutes.js
@@ -1,15 +1,23 @@
-const express = require('express');
+const express = require("express");
+const axios = require("axios");
 const router = express.Router();
-const Movie = require('../models/Movie');
 
-// GET /api/movies/:genre
-router.get('/:genre', async (req, res) => {
+const OMDB_API_KEY = process.env.OMDB_API_KEY; // Load from .env
+
+// Example: GET /api/movies/search?title=Inception
+router.get("/search", async (req, res) => {
+  const { title, imdb } = req.query;
+
+  let url = `http://www.omdbapi.com/?apikey=${OMDB_API_KEY}`;
+  if (imdb) url += `&i=${imdb}`;
+  else if (title) url += `&t=${title}`;
+  else return res.status(400).json({ error: "Provide title or imdb ID" });
+
   try {
-    const genre = req.params.genre;
-    const movies = await Movie.find({ genre: genre });
-    res.json(movies);
-  } catch (err) {
-    res.status(500).json({ message: err.message });
+    const response = await axios.get(url);
+    res.json(response.data);
+  } catch (error) {
+    res.status(500).json({ error: "Failed to fetch movie" });
   }
 });
 
